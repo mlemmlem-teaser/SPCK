7e30d6a6dc9859c6557a6ca16c3a83d92c00d841
mlemmlem_teaser <masteryibm@gmail.com>
2024-01-04 20:28:06 +0700

    sadfd

--- src/main.js
+++ src/main.js
@@ -1,18 +1,20 @@
 const arrow_symbol = () => {
-    const a = document.getElementsByClassName("arrow_symbol");
-    for (let i = 0; i < a.length; i++) {
-        let b = a[i].innerText;
-        a[i].addEventListener("mouseenter", ()=> {
-            a[i].innerText="➙"+b;
-            console.log(a[i])
-        })
-        a[i].addEventListener("mouseleave", ()=> {
-            a[i].innerText=b;
-        })
-    }
-}
+  const a = document.getElementsByClassName("arrow_symbol");
+  for (let i = 0; i < a.length; i++) {
+    let b = a[i].innerText;
+    a[i].addEventListener("mouseenter", () => {
+      a[i].innerText = "➙" + b;
+      console.log(a[i]);
+    });
+    a[i].addEventListener("mouseleave", () => {
+      a[i].innerText = b;
+    });
+  }
+};
 arrow_symbol();
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+  scrollFunction();
+};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -29,3 +31,46 @@ function topFunction() {
     setTimeout(topFunction, 10);
   }
 }
+const url = "https://dogbreeddb.p.rapidapi.com/paginated/";
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "02e21a2917mshb9f7023387f37a0p16f9b8jsnf44ab3717559",
+    "X-RapidAPI-Host": "dogbreeddb.p.rapidapi.com",
+  },
+};
+//test api 1
+fetch(url, options)
+  .then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error("Lỗi: " + response.status);
+    }
+  })
+  .then((data) => {
+    console.log(data);
+  });
+//test api 2
+const url2 = 'https://dog-breed-classification-api.p.rapidapi.com/dog_breed_classification?url=https%3A%2F%2Fwww.petplan.co.uk%2Fimages%2Fbreed-info%2Ffrench-bulldog%2FVital-Stats_French-Bulldog-Petplan.png';
+const options2 = {
+	method: 'POST',
+	headers: {
+		'X-RapidAPI-Key': '02e21a2917mshb9f7023387f37a0p16f9b8jsnf44ab3717559',
+		'X-RapidAPI-Host': 'dog-breed-classification-api.p.rapidapi.com'
+	}
+};
+fetch(url2, options2)
+  .then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error("Lỗi: " + response.status);
+    }
+  })
+  .then((data) => {
+    console.log(data);
+  });
+
+
+
