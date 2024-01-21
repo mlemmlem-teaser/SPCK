2bd314825706d0300878d8bc1bbfe96f3e9c67ae
mlemmlem_teaser <masteryibm@gmail.com>
2024-01-21 14:07:43 +0700

    ádasa

--- src/screens/product/main.js
+++ src/screens/product/main.js
@@ -46,7 +46,7 @@ const api=async ()=> {
             //1111111111
           const items1 = document.createElement("div"); items1.classList.add("items-1");
             const img=document.createElement("img");
-            const items_infor = document.createElement("div");items_infor.classList.add("teams-info");
+            const items_infor = document.createElement("div");items_infor.classList.add("items-info");
                 const items_info_h1 = document.createElement("h1");
                 const items_info_p1 = document.createElement("p");
                 const items_info_p2 = document.createElement("p");
@@ -60,6 +60,32 @@ const api=async ()=> {
                 const buy_btn = document.createElement("button"); buy_btn.classList.add("buy-btn");
                 const cart_btn = document.createElement("button");cart_btn.classList.add("cart-btn");
                     const cart_icon = document.createElement("i"); cart_icon.classList.add("fa"); cart_icon.classList.add("fa-shopping-cart");
+            // More infor
+        const more_infor = document.createElement("div"); more_infor.classList.add("more-infor");
+        const grid_container = document.createElement("div"); grid_container.classList.add("grid-container");
+        const grid_item1 = document.createElement("div"); grid_item1.classList.add("grid-item");
+        const grid_item2 = document.createElement("div"); grid_item2.classList.add("grid-item");
+        const grid_item3 = document.createElement("div"); grid_item3.classList.add("grid-item");
+        const grid_item4 = document.createElement("div"); grid_item4.classList.add("grid-item");
+          const MaxHI= document.createElement("p"); MaxHI.innerText="Max height(inches): "+ maxHeightInches; 
+          const MinHI= document.createElement("p"); MinHI.innerText="Min height(inches): "+ minHeightInches;
+          const MaxLS= document.createElement("p"); MaxLS.innerText="Max life span: " + maxLifeSpan;
+          const MinLS= document.createElement("p"); MinLS.innerText="Min life span: " + minLifeSpan;
+          const MaxW= document.createElement("p");  MaxW.innerText="Max weight(pounds): " + maxWeightPounds;
+          const MinW= document.createElement("p"); MinW.innerText="Min weight(pounds): " + minWeightPounds;
+          const Org= document.createElement("p"); Org.innerText="Origin: "+ origin;
+            grid_item1.appendChild(MaxHI);
+            grid_item1.appendChild(MinHI);
+            grid_item2.appendChild(MaxLS);
+            grid_item2.appendChild(MinLS);
+            grid_item3.appendChild(MaxW);
+            grid_item3.appendChild(MinW);
+            grid_item4.appendChild(Org);
+            grid_container.appendChild(grid_item1);
+            grid_container.appendChild(grid_item2);
+            grid_container.appendChild(grid_item3);
+            grid_container.appendChild(grid_item4);
+            more_infor.appendChild(grid_container);
 
         // gắn giá trị cho đống item
         //"../../../Assets/images/ảnh cheems/cheems.jpg"
@@ -68,24 +94,30 @@ const api=async ()=> {
         items_info_h1.style="font-size: 20px;margin-bottom: 5%;";
         items_info_p1.innerText="BreedType: "+breedType;
         items_info_p2.innerText="FurColor: "+furColor;
-        items_info_p1.style.fontSize="12px";
-        items_info_p2.style.fontSize="12px";
+        items_info_p1.style.fontSize="20px";
+        items_info_p2.style.fontSize="20px";
         items_2_p.innerText=breedDescription;
         buy_btn.innerText="More information";
         // sự kiện cho đống items
         buy_btn.addEventListener("click",()=>{
         const all_items= document.getElementsByClassName("items");
+        const all_more_infor = document.getElementsByClassName("more-infor");
           for (let i = 0; i < all_items.length; i++) {
-            all_items[i].style="  width: 25vw;height: 70vh;box-shadow: 3px 3px 30px 5px rgb(0, 0, 0,0.35);padding: 1%;margin-bottom: 5vh;word-break: break-word;border-radius: 10px;position:inherit;";
+            all_items[i].style="  width: 35vw;height: 70vh;box-shadow: 3px 3px 30px 5px rgb(0, 0, 0,0.35);padding: 1%;margin-bottom: 5vh;word-break: break-word;border-radius: 10px;position:inherit;";
+            all_more_infor[i].style.display="none";
+            items_info_h1.style.fontSize="20px";
           }
           items.style="  z-index: 9999;background-color: white;width: 100vw;height: 90vh;box-shadow: 3px 3px 30px 5px rgb(0, 0, 0,0.35);padding: 1%;margin-bottom: 5vh;word-break: break-word;border-radius: 10px;margin: 1%;display: flex;flex-direction: column;";
-          items1.style="height: calc(1/2.5 * 100%);display:flex;width:60%;padding-left";
+          items1.style="height: 55%;width:100%;padding-left;display:flex;justify-content: space-around;";
+          more_infor.style.display="inherit";
+          items_info_h1.style="font-size: 30px;margin-bottom: 5%;text-align:center;";
         });
         cart_btn.title="Thêm vào giỏ hàng";
         // appendChild 1 đống
         items_infor.appendChild(items_info_h1);
         items_infor.appendChild(items_info_p1);
         items_infor.appendChild(items_info_p2);
+        items_infor.appendChild(more_infor);
         items1.appendChild(img);
         items1.appendChild(items_infor);
         items2.appendChild(items_2_p);
