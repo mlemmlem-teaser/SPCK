4643dca6e71f2eb019dd27873e74ea92e4cea86e
mlemmlem_teaser <masteryibm@gmail.com>
2024-01-26 19:34:52 +0700

    aaa

--- src/main.js
+++ src/main.js
@@ -17,21 +17,7 @@ window.onscroll = function () {
   scrollFunction();
 };
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("scrollBtn").style.display = "block";
-  } else {
-    document.getElementById("scrollBtn").style.display = "none";
-  }
-}
 
-function topFunction() {
-  var position = document.body.scrollTop || document.documentElement.scrollTop;
-  if (position > 0) {
-    window.scrollBy(0, -100);
-    setTimeout(topFunction, 10);
-  }
-}
 // const url = "https://dogbreeddb.p.rapidapi.com/paginated/";
 // const options = {
 //   method: "GET",
